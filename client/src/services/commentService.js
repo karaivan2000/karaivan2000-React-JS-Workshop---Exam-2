5ce45a90938f1131b06c03b172175696bda35dde
karaivan2000 <133887129+karaivan2000@users.noreply.github.com>
2024-08-28 11:30:42 +0300

    Migrate to collections from jsonstore

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -1,6 +1,6 @@
 import * as request from "../lib/request";
 
-const baseUrl = `http://localhost:3030/jsonstore/comments`;
+const baseUrl = `http://localhost:3030/data/comments`;
 
 export const create = async (recipeId, username, text) => {
     const newComment = await request.post(baseUrl, {
--- client/src/services/recipeService.js
+++ client/src/services/recipeService.js
@@ -1,11 +1,11 @@
 import * as request from "../lib/request";
 
-const baseUrl = `http://localhost:3030/jsonstore/recipes`;
+const baseUrl = `http://localhost:3030/data/recipes`;
 
 export const getAll = async () => {
     const result = await request.get(baseUrl);
 
-    return Object.values(result); 
+    return result; 
 };
 
 export const getOne = async (recipeId) => {
